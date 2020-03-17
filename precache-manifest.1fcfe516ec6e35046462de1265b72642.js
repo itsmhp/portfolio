c7393389dd2ee9a993e0d593170c5b0d838e6151
Hanif Pratama <mhp.hanif5@gmail.com>
2020-03-17 18:36:10 +0700

    Updates

---
+++ precache-manifest.1fcfe516ec6e35046462de1265b72642.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41d54f98cbc7b67b528d4f8ea15e8289",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "dda6bf481dcf3d6e2001",
+    "url": "/portfolio/static/css/main.b04244df.chunk.css"
+  },
+  {
+    "revision": "08cd1ac7ef520700efd5",
+    "url": "/portfolio/static/js/2.eabf0129.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/portfolio/static/js/2.eabf0129.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dda6bf481dcf3d6e2001",
+    "url": "/portfolio/static/js/main.8390c4d4.chunk.js"
+  },
+  {
+    "revision": "7107900f59be1fd95810",
+    "url": "/portfolio/static/js/runtime-main.af0b07c5.js"
+  },
+  {
+    "revision": "211dcdbc14d36b01a92788153c7141c5",
+    "url": "/portfolio/static/media/alcatraz-black.211dcdbc.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
